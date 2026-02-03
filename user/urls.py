614b807fa508fccec3da0ccf2e2d3dfe70437806
MPUDirk <p2304362@mpu.edu.mo>
2026-02-03 21:35:44 +0800

    fix01:Improve the auth & registration function

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 
-from .views import SignUpView
+from .views import OSSLoginView, SignUpView
+
+app_name = 'user'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', OSSLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,24 @@
-from django.views.generic import CreateView
-
-from .forms import SignUpForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, FormView, FormMixin
 
 # Create your views here.
-class SignUpView(CreateView):
+class CustomFormMixin(FormMixin):
+    def form_invalid(self:FormView, form:forms.ModelForm):
+        return self.render_to_response({"errors": form.errors.as_data()})
+
+class OSSLoginView(LoginView, CustomFormMixin):
+    template_name = "registration/login.html"
+
+class SignUpView(CreateView, CustomFormMixin):
+    model = User
     template_name = "registration/signup.html"
-    form_class = SignUpForm
+    fields = ['first_name', 'last_name', 'username', 'email', 'password']
+    success_url = reverse_lazy('user:login')
+
+    def form_valid(self, form:forms.ModelForm):
+        User.objects.create_user(**form.cleaned_data)
+        return HttpResponseRedirect(self.success_url)
