46eca788cc031a3f1a7809707108ce0d22ef98c8
Tggtb <syy724337@gmail.com>
2026-03-16 16:12:18 +0800

    3.16.6 商家可以管理自己的商品

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductCreateView, ProductUpdateView
+from .views import ProductCreateView, ProductUpdateView, VendorProductListView
 
 app_name = 'vendor'
 
 urlpatterns = [
+    path('', VendorProductListView.as_view(), name='home'),
     path('products/add/', ProductCreateView.as_view(), name='product_add'),
     path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, ListView, UpdateView
 
 from OnlineShoppingSys.modules import CustomLoginRequiredMixin
 from shopping.models import Product
@@ -28,11 +29,35 @@ class ProductEditPermissionMixin(CustomLoginRequiredMixin, UserPassesTestMixin):
         return product.created_by_id == self.request.user.id
 
 
+class VendorProductListView(VendorOrAdminRequiredMixin, ListView):
+    """Vendor homepage: list products created by the current user (or all for admin)."""
+    model = Product
+    template_name = 'vendor/home.html'
+    context_object_name = 'products'
+    paginate_by = 20
+
+    def get_queryset(self):
+        qs = Product.objects.select_related('category')
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            qs = qs.all()
+        else:
+            qs = qs.filter(created_by=self.request.user)
+        q = self.request.GET.get('q', '').strip()
+        if q:
+            qs = qs.filter(Q(name__icontains=q) | Q(product_id__icontains=q))
+        return qs.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
+
+
 class ProductCreateView(VendorOrAdminRequiredMixin, CreateView):
     model = Product
     form_class = ProductCreateForm
     template_name = 'vendor/product_add.html'
-    success_url = reverse_lazy('shopping:product_list')
+    success_url = reverse_lazy('vendor:home')
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
