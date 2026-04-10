99a8b2c1bc2d037f10e3b2ac71597465c27cae8f
MPUDirk <p2304362@mpu.edu.mo>
2026-04-10 13:48:12 +0800

    Test: SEO

--- user/sitemaps.py
+++ user/sitemaps.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 class UserSitemap(Sitemap):
     def items(self):
-        return ['user:login', 'user:logout', 'user:signup']
+        return ['user:login', 'user:signup']
 
     def location(self, item):
         return reverse(item)
