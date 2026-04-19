29d89d6d86111a1d6729bd2b5d1866d6973cadba
MPUDirk <p2304362@mpu.edu.mo>
2026-04-19 22:28:37 +0800

    fix02: Fix some bug

--- OnlineShoppingSys/views.py
+++ OnlineShoppingSys/views.py
@@ -1,34 +1,42 @@
+import os
 import bs4
 import requests
 
 from django.urls import reverse
 from django.views.generic import TemplateView
-import requests
 
 
 class XSSExampleView(TemplateView):
     template_name = 'xss_example.html'
 
     def get(self, request, *args, **kwargs):
-        baseurl = f'{request.scheme}://{request.get_host()}'
+        baseurl = f'http://127.0.0.1:8000' if not os.environ.get('DEBUG', False) else 'https://isp.dc-yan.top'
         login_url = f'{baseurl}{reverse("user:login")}'
         session = requests.Session()
-        soup = bs4.BeautifulSoup(session.get(login_url).content, 'html.parser')
+        soup = bs4.BeautifulSoup(session.get(login_url,).content, 'html.parser')
         csrftoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
-        session.post(login_url, data={
-            'csrfmiddlewaretoken': csrftoken,
-            'username': 'tuser',
-            'password': 'tuser1234'
-        })
+        session.post(
+            login_url,
+            data={
+                'csrfmiddlewaretoken': csrftoken,
+                'username': 'tuser',
+                'password': 'tuser1234'
+            },
+            headers={'referer': login_url,}
+        )
 
         order_url = f'{baseurl}{reverse("vendor:order_list")}'
         soup = bs4.BeautifulSoup(session.get(order_url).content, 'html.parser')
         csrftoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
         order_update_url = f'{baseurl}{reverse("vendor:order_update", args=[self.kwargs["oid"]])}'
-        session.post(order_update_url, data={
+        session.post(
+            order_update_url,
+            data={
             'csrfmiddlewaretoken': csrftoken,
             'status': 'delivered'
-        })
+            },
+            headers={'referer': order_url,}
+        )
 
         return super().get(request, *args, **kwargs)
 
